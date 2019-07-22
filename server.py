bb3e2c1f140e0852d0a2c6141ea4eda2cb6a62db
Agnes Kadlot <agnes.kadlot@gmail.com>
2019-07-22 21:15:12 +0200

    Add route_question

--- server.py
+++ server.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
+import data_manager
 
 app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/question/<question_id>')
+def route_question(question_id):
+    row = data_manager.get_data_by_key('sample_data/question.csv', question_id, 'id')
+    answers = data_manager.get_data_by_key('sample_data/answer.csv', question_id, 'question_id')
+    return render_template('display_question.html', question=row, answers=answers)
 
 
 @app.route('/add-question', methods=['GET', 'POST'])
