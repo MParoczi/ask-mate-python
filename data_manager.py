78e157070a109a6ad80b12fcb5726186775ce001
Agnes Kadlot <agnes.kadlot@gmail.com>
2019-07-22 21:17:40 +0200

    Add get_data_by_key function

--- data_manager.py
+++ data_manager.py
@@ -1,1 +1,10 @@
+import connection
 
+
+def get_data_by_key(file_name, searching_key, key_to_search):
+    data = connection.read_file(file_name)
+    result = []
+    for row in data:
+        if row[key_to_search] == searching_key:
+            result.append(row)
+    return result
