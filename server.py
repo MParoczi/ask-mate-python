b4c5599a718a169172099c6f0ddb3c3ad6cd2190
Márk Paróczi <mark.paroczi@gmail.com>
2019-07-22 15:43:38 +0200

    Route '/add-question'

--- server.py
+++ server.py
@@ -3,5 +3,10 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
-if __name__ == "__main__":
+@app.route('/add-question', methods=['GET', 'POST'])
+def add_question():
+    return render_template('add_question.html')
+
+
+if __name__ == '__main__':
     app.run(debug=True)
