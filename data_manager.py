fe9f5302bddb75f3efcea7a68f32bbdabb56fe9e
Márk Paróczi <mark.paroczi@gmail.com>
2019-07-23 11:07:23 +0200

    Add create_new_id function

--- data_manager.py
+++ data_manager.py
@@ -8,3 +8,10 @@ def get_data_by_key(file_name, searching_key, key_to_search):
         if row[key_to_search] == searching_key:
             result.append(row)
     return result
+
+
+def create_new_id(file_name):
+    NEW_ID = 1
+    data = connection.read_file(file_name)
+    highest_id = max(data, key=lambda x: int(x['id']))['id']
+    return highest_id + NEW_ID
