0da6b6488931ea71fd66e67233039625895b07c8
kadartibi <kadart78@gmail.com>
2019-07-23 11:22:53 +0200

    List reversed

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/list')
 def route_list():
-    questions = connection.read_file('sample_data/question.csv')
+    questions = reversed(connection.read_file('sample_data/question.csv'))
     return render_template('list.html', questions=questions)
 
 
